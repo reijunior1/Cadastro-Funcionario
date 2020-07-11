ecb3c06a3bf8ab0de4c0c13bc43fc02d825c3554
Reinado <reinaldo.couto.ext@sascar.com.br>
2020-07-11 15:54:18 -0300

    primeiro commit projeto

---
+++ src/main/java/com/algaworks/cadastro/CadastroFuncionarioApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.cadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroFuncionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroFuncionarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/cadastro/CadastroFuncionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroFuncionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
